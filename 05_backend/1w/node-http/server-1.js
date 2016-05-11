1c21a35d5171463c2bb7aac862fd13c55c6300ec
Esperanza <hola@mi-mina.com>
2016-05-11 20:39:11 +0200

    node server

--- 05_backend/1w/node-http/server-1.js
+++ 05_backend/1w/node-http/server-1.js
@@ -1,13 +1,13 @@
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var server = http.createServer(function (req, res) {
+  console.log(req.headers);
 
-
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello World</h1>');
 });
 
 server.listen(port, hostname, function(){
