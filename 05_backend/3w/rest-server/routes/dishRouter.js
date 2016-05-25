0a7e66b72d74cc25e43d5c78deb089ac763f8061
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-05-25 21:07:39 +0200

    exercice express+mongodb

--- 05_backend/3w/rest-server/routes/dishRouter.js
+++ 05_backend/3w/rest-server/routes/dishRouter.js
@@ -1,29 +1,44 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
-var router = express.Router()
+    bodyParser = require('body-parser')
+    mongoose = require('mongoose');
+
+var Dishes = require('../models/dishes');
+
+var dishRouter = express.Router()
     .use(bodyParser.json());
 
-router.route('/')
-  .all(function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    next();
-  })
+dishRouter.route('/')
   .get(function (req, res, next) {
-    res.end('Will send all the dishes to you!');
+    // Dishes.find takes 2 arguments. The first arguments is the object we are
+    // looking for. In thes case, as it is an empty object, we are asking for
+    // all the elements. We will get this back as an array.
+    // The second argument is the callback function
+    Dishes.find({}, function (err, dish) {
+      if (err) throw err;
+      res.json(dish);
+      // We send the response back as a json string
+      // We don't need to add the headers, or the code, they will be added
+      // automatically
+    });
   })
   .post(function (req, res, next) {
-    res.end('Will add the dish: '+ req.body.name + ' with details: ' +
-    req.body.description);
+    // Dishes.create takes as first parameter the object to be inserted in the
+    // mongoDB. And a callback function as second parameter.
+    Dishes.create(req.body, function (err, dish) {
+      if (err) throw err;
+      console.log('Dish created!');
+      var id = dish._id;
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Added the dish with id: '+ id);
+    });
   })
   .delete(function (req, res, next) {
     res.end('Deleting all dishes');
   });
 
-router.route('/:dishId')
-  .all(function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    next();
-  })
+dishRouter.route('/:dishId')
   .get(function (req, res, next) {
     res.end('Will send details of the dish: ' + req.params.dishId +
     ' to you!');
@@ -37,4 +52,4 @@ router.route('/:dishId')
     res.end('Deleting dish: ' + req.params.dishId);
   });
 
-module.exports = router;
+module.exports = dishRouter;
