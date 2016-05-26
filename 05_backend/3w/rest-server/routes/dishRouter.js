602a912d6049201e606b3a41db597ae8f38259b7
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-05-26 17:34:16 +0200

    rest-server

--- 05_backend/3w/rest-server/routes/dishRouter.js
+++ 05_backend/3w/rest-server/routes/dishRouter.js
@@ -9,8 +9,11 @@ var dishRouter = express.Router()
 
 dishRouter.route('/')
   .get(function (req, res, next) {
+    // Dishes is a model. Methods available to models are in model.js in the
+    // mongoose API.
     // Dishes.find takes 2 arguments. The first arguments is the object we are
-    // looking for. In thes case, as it is an empty object, we are asking for
+    // looking for. It's an object with conditions (like a filter)
+    // In this case, as it is an empty object, we are asking for
     // all the elements. We will get this back as an array.
     // The second argument is the callback function
     Dishes.find({}, function (err, dish) {
@@ -35,21 +38,107 @@ dishRouter.route('/')
     });
   })
   .delete(function (req, res, next) {
-    res.end('Deleting all dishes');
+    // If we invoke Dishes.remove whith an empty object as a first parameter,
+    // we will delete all the items in the collection
+    // The resp of the callback function will be a javascript object thal will
+    // indicate how many objects has been deleted
+    Dishes.remove({}, function (err, resp) {
+      if (err) throw err;
+      res.json(resp);
+    });
   });
 
 dishRouter.route('/:dishId')
   .get(function (req, res, next) {
-    res.end('Will send details of the dish: ' + req.params.dishId +
-    ' to you!');
+    // The Id comes as a parameter and with have access to it by req.params.dishId
+    Dishes.findById(req.params.dishId, function (err, dish) {
+        if (err) throw err;
+        res.json(dish);
+    });
   })
   .put(function (req, res, next) {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-    ' with details: ' + req.body.description);
+    // My expectation is that the update information comes in the body of the message
+    // that's why {$set: req.body}
+    // new: true -> true to return the modified document rather than the original.
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, dish) {
+        if (err) throw err;
+        res.json(dish);
+    });
   })
   .delete(function(req, res, next){
-    res.end('Deleting dish: ' + req.params.dishId);
+    Dishes.findByIdAndRemove(req.params.dishId, function (err, resp) {
+      if (err) throw err;
+      res.json(resp);
+    });
+  });
+
+  dishRouter.route('/:dishId/comments')
+  .get(function (req, res, next) {
+      Dishes.findById(req.params.dishId, function (err, dish) {
+          if (err) throw err;
+          res.json(dish.comments);
+      });
+  })
+  .post(function (req, res, next) {
+      Dishes.findById(req.params.dishId, function (err, dish) {
+          if (err) throw err;
+          dish.comments.push(req.body);
+          dish.save(function (err, dish) {
+              if (err) throw err;
+              console.log('Updated Comments!');
+              res.json(dish);
+          });
+      });
+  })
+  .delete(function (req, res, next) {
+      Dishes.findById(req.params.dishId, function (err, dish) {
+          if (err) throw err;
+          for (var i = (dish.comments.length - 1); i >= 0; i--) {
+              dish.comments.id(dish.comments[i]._id).remove();
+          }
+          dish.save(function (err, result) {
+              if (err) throw err;
+              res.writeHead(200, {
+                  'Content-Type': 'text/plain'
+              });
+              res.end('Deleted all comments!');
+          });
+      });
+  });
+
+  dishRouter.route('/:dishId/comments/:commentId')
+  .get(function (req, res, next) {
+      Dishes.findById(req.params.dishId, function (err, dish) {
+          if (err) throw err;
+          res.json(dish.comments.id(req.params.commentId));
+      });
+  })
+  .put(function (req, res, next) {
+      // We delete the existing commment and insert the updated
+      // comment as a new comment
+      Dishes.findById(req.params.dishId, function (err, dish) {
+          if (err) throw err;
+          dish.comments.id(req.params.commentId).remove();
+          dish.comments.push(req.body);
+          dish.save(function (err, dish) {
+              if (err) throw err;
+              console.log('Updated Comments!');
+              res.json(dish);
+          });
+      });
+  })
+  .delete(function (req, res, next) {
+      Dishes.findById(req.params.dishId, function (err, dish) {
+          dish.comments.id(req.params.commentId).remove();
+          dish.save(function (err, resp) {
+              if (err) throw err;
+              res.json(resp);
+          });
+      });
   });
 
 module.exports = dishRouter;
--- 05_backend/3w/rest-server/routes/leaderRouter.js
+++ 05_backend/3w/rest-server/routes/leaderRouter.js
@@ -1,40 +1,59 @@
 var express = require('express'),
-    bodyParser = require('body-parser');
-var router = express.Router()
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+var Leaders = require('../models/leadership');
+
+var leadersRouter = express.Router()
     .use(bodyParser.json());
 
-router.route('/')
-  .all(function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    next();
-  })
+leadersRouter.route('/')
   .get(function (req, res, next) {
-    res.end('Will send all the leaders to you!');
+    Leaders.find({}, function (err, leader) {
+      if (err) throw err;
+      res.json(leader);
+    });
   })
   .post(function (req, res, next) {
-    res.end('Will add the leader: '+ req.body.name + ' with details: ' +
-    req.body.description);
+    Leaders.create(req.body, function (err, leader) {
+      if (err) throw err;
+      console.log('Leader created!');
+      var id = leader._id;
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Added the leader with id: '+ id);
+    });
   })
   .delete(function (req, res, next) {
-    res.end('Deleting all leaders');
+    Leaders.remove({}, function (err, resp) {
+      if (err) throw err;
+      res.json(resp);
+    });
   });
 
-router.route('/:leaderId')
-  .all(function (req, res, next) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    next();
-  })
+leadersRouter.route('/:leaderId')
   .get(function (req, res, next) {
-    res.end('Will send details of the leader: ' + req.params.leaderId +
-    ' to you!');
+    Leaders.findById(req.params.leaderId, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
   })
   .put(function (req, res, next) {
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name +
-    ' with details: ' + req.body.description);
+    Leaders.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function (err, leader) {
+        if (err) throw err;
+        res.json(leader);
+    });
   })
   .delete(function(req, res, next){
-    res.end('Deleting leader: ' + req.params.leaderId);
+    Leaders.findByIdAndRemove(req.params.leadersId, function (err, resp) {
+      if (err) throw err;
+      res.json(resp);
+    });
   });
 
-module.exports = router;
+module.exports = leadersRouter;
