a7e242667a4855494d5ef1ad69bcc2a49a1b95ea
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-05-28 20:39:53 +0200

    mongoose population

---
+++ 05_backend/4w/mongoose-population/models/user.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Supongo que lo anterior se puede compactar en una sola línea así:
+// var Schema = require('mongoose').Schema;
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+  username: String,
+  password: String,
+  firstname: {
+    type: String,
+    default: ''},
+  lastname: {
+    type: String,
+    default: ''},
+  admin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+User.methods.getName = function () {
+  return (this.firstname + ' ' + this.lastname);
+};
+
+User.plugin(passportLocalMongoose);
+// we apply the passport-local-mongoose plugin to our User Schema
+// Then we will be able to use the methods provided by passport-local-mongoose
+// like register, authenticate, serializeUser, findByUsername...
+
+module.exports = mongoose.model('User', User);
