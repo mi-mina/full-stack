ff750576f008b836e430b80394b02f47fdb113ed
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-05-29 19:28:29 +0200

    Oauth

---
+++ 05_backend/4w/Oauth/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '242176549486691',
+        clientSecret: 'c2f533effc15b661159ab223d7d30017',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
