62c6c9fc1df7a8ada2aabf2058eda23da1705a03
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-06-02 22:39:57 +0200

    tarea

--- 05_backend/4w/tarea/routes/favoriteRouter.js
+++ 05_backend/4w/tarea/routes/favoriteRouter.js
@@ -19,80 +19,55 @@ favoriteRouter.route('/')
   })
   .post(Verify.verifyOrdinaryUser, function (req, res, next) {
     req.body.postedBy = req.decoded._doc._id;
+    req.body.dishes = req.body._id;
 
-      req.body.dishes = req.body._id;
-
-      Favorites.create(req.body, function (err, favorite) {
-          if (err) throw err;
-          console.log('Favorite dish created!');
-          res.json(favorite);
-        });
-
-    // Favorites.find({postedBy: req.body.postedBy})
-    //   .exec(function (err, favorites) {
-    //     if (err) throw err;
-    //     res.json(favorites);
-    //     // if (favorites.dishes.indexOf(req.body._id) < 0) {
-    //     //   req.body.dishes.push(req.body._id);
-    //     //
-    //     //   Favorites.create(req.body, function (err, favorite) {
-    //     //       if (err) throw err;
-    //     //       console.log('Favorite dish created!');
-    //     //       res.json(favorite);
-    //     //     });
-    //     // } else {
-    //     //   console.log("Favorite dish already exists.");
-    //     // };
-    //
-    //
-    //   });
-
-
-
-// When the user does a POST operation on '/favorites' by including
-// {"_id":"dish ObjectId"} in the body of the message, you will
-// (a) create a favorites document if such a document corresponding to this
-// user does not already exist in the system,
-// (b) add the dish specified in the body of the message to the list of favorite
-// dishes for the user, if the dish does not already exists in the list of favorites.
-
+    Favorites.find({postedBy: req.body.postedBy})
+      .exec(function (err, myFavorites) {
+        if (err) throw err;
+        if (myFavorites[0]) {
+          console.log('voy a actualizar un favorite existente');
+          console.log(myFavorites);
+          console.log(myFavorites[0]);
+          if (myFavorites[0].dishes.indexOf(req.body._id) < 0) {
+            myFavorites[0].dishes.push(req.body._id);
+            myFavorites[0].save(function (err, favorite) {
+              if (err) throw err;
+              console.log('Favorite updated!');
+              console.log(myFavorites);
+              console.log(favorite);
+              res.send(favorite);
+            });
+          } else {
+            res.send('Ya has añadido ese plato a favoritos');
+          };
+        } else {
+          console.log('voy a crear un favorites nuevo');
+          console.log(req.body); //el body tiene un id = que el del plato por que?
+          // yo quiero crear un elemento con una id unica cada vez
+          Favorites.create(req.body, function (err, favorite) {
+            if (err) throw err;
+            console.log(req.body);
+            console.log('Favorite dish created!');
+            res.json(favorite);
+          });
+        };
+      });
     });
-  //
-  // .delete();
-  //
-  // Dishes.findById(req.params.dishId, function (err, dish) {
-  //     if (err) throw err;
-  //     req.body.postedBy = req.decoded._doc._id;
-  //     // What we get in the req.body is the information to create a new comment.
-  //     // We assign the property .postedBy the id of the user passed through the token
-  //     // before we push the req.body to the array of comments.
-  //     dish.comments.push(req.body);
-  //     dish.save(function (err, dish) {
-  //         if (err) throw err;
-  //         console.log('Updated Comments!');
-  //         res.json(dish);
-  //     });
-  // });
 
+    favoriteRouter.route('/:id')
+      .get(Verify.verifyOrdinaryUser, function (req, res, next) {
+        Favorites.find({postedBy: req.params.id})
+          .exec(function (err, favorite) {
+            if (err) throw err;
+            res.json(favorite);
+        });
+      });
+
+    // When the user does a POST operation on '/favorites' by including
+    // {"_id":"dish ObjectId"} in the body of the message, you will
+    // (a) create a favorites document if such a document corresponding to this
+    // user does not already exist in the system,
+    // (b) add the dish specified in the body of the message to the list of favorite
+    // dishes for the user, if the dish does not already exists in the list of favorites.
 
 module.exports = favoriteRouter;
-
-
-    // dishRouter.route('/')
-    //       .post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-    //     Dishes.create(req.body, function (err, dish) {
-    //       if (err) throw err;
-    //       console.log('Dish created!');
-    //       var id = dish._id;
-    //       res.writeHead(200, {
-    //         'Content-Type': 'text/plain'
-    //       });
-    //       res.end('Added the dish with id: '+ id);
-    //     });
-    //   })
-    //   .delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-    //     Dishes.remove({}, function (err, resp) {
-    //       if (err) throw err;
-    //       res.json(resp);
-    //     });
-    //   });
---
+++ 05_backend/additionalstuff/pruebas.js
@@ -0,0 +1,12 @@
+var array = {};
+
+
+if (array[0])
+  {
+    console.log('1')
+  }
+else {
+  console.log('2');
+};
+
+console.log(array.length);
